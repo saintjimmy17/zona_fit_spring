50dc4f39e4f8e8b554b33fcc6c8e3a330dc4e374
Marian <italianomariano198@gmail.com>
2024-10-09 17:13:55 -0300

    first commit

---
+++ src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -0,0 +1,30 @@
+package gm.zona_fit;
+
+import gm.zona_fit.servicio.IClienteServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonaFitApplication implements CommandLineRunner {
+
+	@Autowired
+	private IClienteServicio clienteServicio;
+
+	private static final Logger logger = LoggerFactory.getLogger(ZonaFitApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("Iniciando la aplicacion");
+		//Levantar la fabrica de Spring
+		SpringApplication.run(ZonaFitApplication.class, args);
+		logger.info("Aplicacion Finalizada!");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("*** Aplicacion Zona Fit (GYN) ***");
+	}
+}
